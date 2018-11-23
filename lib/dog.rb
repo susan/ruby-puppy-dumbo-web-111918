63d04124249d219f370acd1f24f2c0bf41bb7bbb
Susan Isserman <bluenote@outlook.com>
2018-11-23 02:37:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class Dog
+  attr_reader :name
+  @@all = [] #keeps track of puppy count
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+   def self.all
+     @@all.each do |pup|
+       puts "#{pup.name}"
+     end
+   end
+
+   def self.clear_all
+     @@all.clear
+   end
+
+end
+#binding.pry
